148b0fdf10a27306057354d1c5122825a708b1fb
pouya mohtacham <pouyajoon@gmail.com>
2018-09-21 12:19:52 +0200

    clean build

--- ssr-prod.sh
+++ ssr-prod.sh
@@ -4,12 +4,7 @@ yarn ssr-prod
 cp credentials build/server/
 cp data build/server/
 
-
 # ./node_modules/.bin/webpack --config ssr/webpack.config.js 
 # node build/js/server.js
 
 # ./node_modules/.bin/webpack --config ssr/webpack.config.js --watch
-
-# ./node_modules/.bin/webpack-dev-server --config ssr/webpack.config.js --env.platform=server 
-# ./node_modules/.bin/webpack-dev-server --config ssr/webpack.config.js --env.platform=web
-
