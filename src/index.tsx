13320bcf4a38ba0aee27548441810605d8651b9d
pouya mohtacham <pouyajoon@gmail.com>
2018-09-22 16:25:21 +0200

    remove correctly sw

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
 import App from './App';
 
 ReactDOM.hydrate(
   <App />,
   document.getElementById('root') as HTMLElement
 );
-registerServiceWorker();
