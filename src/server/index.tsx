cd00082ac8f9db30c6eb1f8337e2d73ef633ca25
pouya mohtacham <pouyajoon@gmail.com>
2018-10-03 00:10:30 +0200

    Add sitemap

--- src/server/index.tsx
+++ src/server/index.tsx
@@ -11,11 +11,12 @@ import LeftDrawer from '../Components/LeftDrawer/LeftDrawer';
 import { StaticRouter, Switch } from 'react-router';
 import { renderRoutes } from '../Router/Routes';
 import { IHappyTechStore } from '../models';
-import { allRouterPages } from './../Utils/Pages/pages';
+import { allRouterPages, menuPages } from './../Utils/Pages/pages';
 import { Helmet } from 'react-helmet';
 import { startupLinkName } from '../Components/StartupCard';
 import Jimp = require('jimp');
 import { cloudinaryTransform } from '../Utils/Cloudinary';
+import { startupPage } from '../Components/StartupView';
 const port = process.env.PORT || 9000;
 // const port = 9000;
 
@@ -55,8 +56,15 @@ function getReactApp(store: IHappyTechStore, url: string): IReactApp {
     console.info('Start loading store...');
     const store = await getStore();
 
-
     allRouterPages.forEach(page => {
+
+        server.get(`/sitemap.txt`, async (req, res) => {
+            const menus = menuPages().map(p => p.route);
+            const startups = store.startups.map(s => startupPage().route.replace(':name', startupLinkName(s)));
+            let urls: string[] = [];
+            urls = urls.concat(menus, startups)
+            res.send(urls.map(u => `https://www.happytech.life/${u}`).join('\n'));
+        });
         server.get(`/startups/:name/logos/label.png`, async (req, res) => {
 
             const labelUrl = 'https://res.cloudinary.com/happytech/image/upload/v1538084191/logos/label.png';
