8959c61cb307e7d0a3ce995069ee9ee51867ac17
pouya mohtacham <pouyajoon@gmail.com>
2018-10-10 21:42:54 +0200

    try out the images

--- src/server/index.tsx
+++ src/server/index.tsx
@@ -13,13 +13,13 @@ import { renderRoutes } from '../Router/Routes';
 import { IHappyTechStore } from '../models';
 import { allRouterPages, menuPages } from './../Utils/Pages/pages';
 import { Helmet } from 'react-helmet';
-// import Jimp = require('jimp');
-import * as Jimp from 'jimp'
 import { cloudinaryTransform } from '../Utils/Cloudinary';
-import { toImageMime } from './images';
 import { getStartupUrl, getStartup } from '../Utils/startups';
 import { startupPage } from '../Components/StartupView';
+// import { request } from 'http';
 const port = process.env.PORT || 9000;
+// import { request } from 'https';
+import { get } from 'request';
 // const port = 9000;
 
 interface IReactApp {
@@ -48,6 +48,7 @@ function getReactApp(store: IHappyTechStore, url: string): IReactApp {
     const css = sheetsRegistry.toString();
     return { body, css };
 }
+const urlTo64 = (url: string) => Buffer.from(url).toString('base64');
 
 
 (async () => {
@@ -69,39 +70,21 @@ function getReactApp(store: IHappyTechStore, url: string): IReactApp {
             urls = urls.concat(menus, startups)
             res.send(urls.map(u => `https://www.happytech.life/${u}`).join('\n'));
         });
-        server.get(`/startups/:name/logos/label.png`, async (req, res) => {
-            const labelUrl = 'https://res.cloudinary.com/happytech/image/upload/v1538084191/logos/label.png';
-            console.log(req.params);
-            const { name } = req.params;
-            const startup = getStartup(store, name);
-            if (startup) {
-                const logoUrl = cloudinaryTransform(startup.iconUrl, 'w_650,h_650,c_scale,f_png,bg_white');
-                const icon = await Jimp.read(logoUrl);
-                // icon = icon.scaleToFit(650, 650, Jimp.RESIZE_BEZIER);
-                let label = await Jimp.read(labelUrl);
-                label = label.composite(icon, 1170, 128);
-                return toImageMime(res, icon);
-            }
-        })
+
 
         server.get(`/startups/images/summit/:name.png`, async (req, res) => {
-
             try {
-                const labelUrl = cloudinaryTransform('https://res.cloudinary.com/happytech/image/upload/v1539173249/website/summit/summit-bg.png', 'w_650,h_650');
-                console.log(req.params);
                 const { name } = req.params;
                 const startup = getStartup(store, name);
                 if (startup) {
-                    const logoUrl = cloudinaryTransform(`${startup.iconUrl}`, 'w_400,h_200,c_pad,f_png,b_white');
-                    console.log(logoUrl);
-                    let icon = await Jimp.read(logoUrl);
-                    icon = icon.scaleToFit(400, 200);
-                    console.log('icon', icon);
-                    let label = await Jimp.read(labelUrl);
-                    label = label.composite(icon, 125, 175);
-                    return toImageMime(res, label);
+                    const logo = cloudinaryTransform(`${startup.iconUrl}`, 'w_400,h_200,c_pad,f_png,b_white');
+                    const logoUrlBase = urlTo64(logo);
+                    const fetch = `l_fetch:${logoUrlBase},g_north_west,x_125,y_175,w_400,h_200`;
+                    const labelUrl = cloudinaryTransform('https://res.cloudinary.com/happytech/image/upload/v1539173249/website/summit/summit-bg.png', `w_650,h_650/${fetch}`);
+                    get(labelUrl).pipe(res);
+                } else {
+                    throw new Error(`startup [${name}] not found : ${JSON.stringify(req.params)}`)
                 }
-                throw new Error(`startup [${name}] not found : ${JSON.stringify(req.params)}`)
             } catch (err) {
                 console.error(err);
                 res.status(500).send(err.message);
