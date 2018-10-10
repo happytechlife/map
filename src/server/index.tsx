5afa1a9b917983bcd27d494a156060def3ecb5e2
pouya mohtacham <pouyajoon@gmail.com>
2018-10-10 23:48:08 +0200

    move all 404 to pages to home

--- src/server/index.tsx
+++ src/server/index.tsx
@@ -59,44 +59,52 @@ const urlTo64 = (url: string) => Buffer.from(url).toString('base64');
     const store = await getStore();
     allRouterPages.forEach(page => {
 
-        server.get(`/robots.txt`, async (req, res) => {
-            res.send('Sitemap: https://www.happytech.life/sitemap.txt');
-        });
-
-        server.get(`/sitemap.txt`, async (req, res) => {
-            const menus = menuPages().map(p => p.route);
-            const startups = store.startups.map(s => getStartupUrl(startupPage().route, s));
-            let urls: string[] = [''];
-            urls = urls.concat(menus, startups)
-            res.send(urls.map(u => `https://www.happytech.life/${u}`).join('\n'));
-        });
-
-
-        server.get(`/startups/images/summit/:name.png`, async (req, res) => {
-            try {
-                const { name } = req.params;
-                const startup = getStartup(store, name);
-                if (startup) {
-                    const logo = cloudinaryTransform(`${startup.iconUrl}`, 'w_400,h_200,c_pad,f_png,b_white');
-                    const logoUrlBase = urlTo64(logo);
-                    const fetch = `l_fetch:${logoUrlBase},g_north_west,x_125,y_175,w_400,h_200`;
-                    const labelUrl = cloudinaryTransform('https://res.cloudinary.com/happytech/image/upload/v1539173249/website/summit/summit-bg.png', `w_650,h_650/${fetch}`);
-                    get(labelUrl).pipe(res);
-                } else {
-                    throw new Error(`startup [${name}] not found : ${JSON.stringify(req.params)}`)
-                }
-            } catch (err) {
-                console.error(err);
-                res.status(500).send(err.message);
-            }
-        })
 
         server.get(`/${page.route}`, async (req, res) => {
             const ra = getReactApp(store, req.url);
             const helmet = Helmet.renderStatic();
             res.send(html({ helmet, store, ...ra }));
         })
+
+
     })
+
+    server.get(`/robots.txt`, async (req, res) => {
+        res.send('Sitemap: https://www.happytech.life/sitemap.txt');
+    });
+
+    server.get(`/sitemap.txt`, async (req, res) => {
+        const menus = menuPages().map(p => p.route);
+        const startups = store.startups.map(s => getStartupUrl(startupPage().route, s));
+        let urls: string[] = [''];
+        urls = urls.concat(menus, startups)
+        res.send(urls.map(u => `https://www.happytech.life/${u}`).join('\n'));
+    });
+
+
+    server.get(`/startups/images/summit/:name.png`, async (req, res) => {
+        try {
+            const { name } = req.params;
+            const startup = getStartup(store, name);
+            if (startup) {
+                const logo = cloudinaryTransform(`${startup.iconUrl}`, 'w_400,h_200,c_pad,f_png,b_white');
+                const logoUrlBase = urlTo64(logo);
+                const fetch = `l_fetch:${logoUrlBase},g_north_west,x_125,y_175,w_400,h_200`;
+                const labelUrl = cloudinaryTransform('https://res.cloudinary.com/happytech/image/upload/v1539173249/website/summit/summit-bg.png', `w_650,h_650/${fetch}`);
+                get(labelUrl).pipe(res);
+            } else {
+                throw new Error(`startup [${name}] not found : ${JSON.stringify(req.params)}`)
+            }
+        } catch (err) {
+            console.error(err);
+            res.status(500).send(err.message);
+        }
+    })
+
+    server.get(`*`, async (req, res) => {
+        res.status(301).redirect('/')
+    })
+
 
     server.listen(port, () => console.log(`Example app listening on port ${port}!`));
 })();
