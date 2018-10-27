ac2e72b41123a8c2cfbcd26ba537800c8db3b1b0
pouya mohtacham <pouyajoon@gmail.com>
2018-10-27 23:15:40 +0200

    update html

--- src/server/html.ts
+++ src/server/html.ts
@@ -25,7 +25,7 @@ const html = ({ helmet, store, body, css }: IHtml) => `
       ${helmet.script.toString()}
       <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
       <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
-      
+      <meta name="google-site-verification" content="27ecIdQDv1hp9H3lsJwap52eAcEcoV3o-Mok0TO7t_c" />
       <link rel="shortcut icon" href="https://res.cloudinary.com/happytech/image/upload/c_scale,w_128/v1534592246/logos/happytech_zoom.ico">
       <script src="https://apis.google.com/js/api.js"></script>
       <script src="https://maps.googleapis.com/maps/api/js?use_slippy=true&key=AIzaSyCYe_0CiU5xTIZ9f3svSZEaaPUjBb0CHpw&libraries=geometry,places"></script>
